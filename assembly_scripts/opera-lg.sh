e296d5e02b7b05f6da97313eda315907a890ed71
julienzhang <zmzhang@life.hkbu.edu.hk>
2022-05-27 13:55:20 +0800

    Update opera-lg.sh

--- assembly_scripts/opera-lg.sh
+++ assembly_scripts/opera-lg.sh
@@ -8,11 +8,20 @@ if [ ! -d $out ]
 then
     mkdir $out
 fi
+
 cd $out
 gzip -dc $long > long_reads.fastq &
-~/code/seqbasic/seqbasic -r $short -o reads &
+pigz -dc $short | seqkit split2 -p 2 - &
+wait
+
+mv stdin.split/* ./
+rm -r stdin.split
+gzip stdin.part_001.fastq &
+gzip stdin.part_002.fastq &
 wait
 cd ..
-read1=`pwd`/$out/reads_1.fq.gz
-read2=`pwd`/$out/reads_2.fq.gz
+
+read1=`pwd`/$out/stdin.part_001.fastq.gz
+read2=`pwd`/$out/stdin.part_002.fastq.gz
 ~/software/OPERA-LG/bin/OPERA-long-read.pl --contig-file $contig --kmer 55 --long-read-file `pwd`/$out/long_reads.fastq --illumina-read1 $read1 --illumina-read2 $read2 --num-of-processors 100 --opera ~/software/OPERA-LG/bin --samtools-dir ~/software/samtools-0.1.19/bin --blasr ~/software/blasr-1.3.1 --output-prefix operalg --output-directory $out
+rm $read1 $read2
