fe9e244954f1f41c5abcd67ef7c947c181975df6
julienzhang <zmzhang@life.hkbu.edu.hk>
2022-05-27 12:57:13 +0800

    Update metaflye.sh

--- assembly scripts/metaflye.sh
+++ assembly scripts/metaflye.sh
@@ -1,3 +1,11 @@
 reads=$1
 output=$2
+
+# ont
 ~/software/anaconda3/envs/metacomp/bin/flye --meta --nano-raw $reads -t 100 -o ${output}_metaflye
+
+# pacbio clr
+# ~/software/anaconda3/envs/metacomp/bin/flye --meta --pacbio-raw $reads -t 100 -o ${output}_metaflye
+
+# pacbio hifi
+# ~/software/anaconda3/envs/metacomp/bin/flye --meta --pacbio-hifi $reads -t 100 -o ${output}_metaflye
