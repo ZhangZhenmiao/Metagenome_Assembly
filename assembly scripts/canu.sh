6fdbf16daae64380d339bbe364b4dcb4de895107
julienzhang <zmzhang@life.hkbu.edu.hk>
2022-05-27 12:55:20 +0800

    Update canu.sh

--- assembly scripts/canu.sh
+++ assembly scripts/canu.sh
@@ -2,5 +2,15 @@ reads=`pwd`/$1
 output=$2
 genome=$3
 mkdir ${output}_canu; cd ${output}_canu
+
+# ont
 echo "~/software/anaconda3/envs/metacomp/bin/canu -p canu -d `pwd` genomeSize="$genome"m -nanopore-raw "$reads" useGrid=false minThreads=2" > canu.sh
 ~/software/anaconda3/envs/metacomp/bin/canu -p canu -d `pwd` genomeSize=${genome}m -nanopore-raw $reads useGrid=false minThreads=2 > canu.log 2>&1
+
+# pacbio clr
+# echo "~/software/anaconda3/envs/metacomp/bin/canu -p canu -d `pwd` genomeSize="$genome"m -pacbio-raw "$reads" useGrid=false minThreads=2" > canu.sh
+# ~/software/anaconda3/envs/metacomp/bin/canu -p canu -d `pwd` genomeSize=${genome}m -pacbio-raw $reads useGrid=false minThreads=2 > canu.log 2>&1
+
+# pacbio hifi
+# echo "~/software/anaconda3/envs/metacomp/bin/canu -p canu -d `pwd` genomeSize="$genome"m -pacbio-hifi "$reads" useGrid=false minThreads=2" > canu.sh
+# ~/software/anaconda3/envs/metacomp/bin/canu -p canu -d `pwd` genomeSize=${genome}m -pacbio-hifi $reads useGrid=false minThreads=2 > canu.log 2>&1
