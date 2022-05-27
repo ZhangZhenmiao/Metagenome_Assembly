719ff220fbb549dae70d6e3c988eb6c277ae204b
julienzhang <zmzhang@life.hkbu.edu.hk>
2022-05-27 13:06:42 +0800

    Update wtdbg2.sh

--- assembly scripts/wtdbg2.sh
+++ assembly scripts/wtdbg2.sh
@@ -3,5 +3,14 @@ genome=$2
 reads=$3
 
 mkdir $output
+
+# ont
 ~/software/anaconda3/envs/metacomp/bin/wtdbg2 -x ont -g ${genome}m -t 100 -i $reads -fo $output/wtdbg2
+
+# pacbio clr
+# ~/software/anaconda3/envs/metacomp/bin/wtdbg2 -x sq -g ${genome}m -t 100 -i $reads -fo $output/wtdbg2
+
+# pacbio hifi
+# ~/software/anaconda3/envs/metacomp/bin/wtdbg2 -x ccs -g ${genome}m -t 100 -i $reads -fo $output/wtdbg2
+
 ~/software/anaconda3/envs/metacomp/bin/wtdbg-cns -t 100 -i $output/wtdbg2.ctg.lay.gz -fo $output/wtdbg2.ctg.fa
