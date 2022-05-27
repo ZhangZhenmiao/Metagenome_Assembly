e3dc21636af5b408102484fb0135b707c55649a5
julienzhang <zmzhang@life.hkbu.edu.hk>
2022-05-27 13:20:12 +0800

    Rename assembly scripts/operalg.sh to assembly_scripts/operalg.sh

---
+++ assembly_scripts/operalg.sh
@@ -0,0 +1,18 @@
+contig=$1
+long=`pwd`/$2
+short=`pwd`/$3
+out=$4
+source ~/.bashrc
+conda deactivate; conda activate python2
+if [ ! -d $out ]
+then
+    mkdir $out
+fi
+cd $out
+gzip -dc $long > long_reads.fastq &
+~/code/seqbasic/seqbasic -r $short -o reads &
+wait
+cd ..
+read1=`pwd`/$out/reads_1.fq.gz
+read2=`pwd`/$out/reads_2.fq.gz
+~/software/OPERA-LG/bin/OPERA-long-read.pl --contig-file $contig --kmer 55 --long-read-file `pwd`/$out/long_reads.fastq --illumina-read1 $read1 --illumina-read2 $read2 --num-of-processors 100 --opera ~/software/OPERA-LG/bin --samtools-dir ~/software/samtools-0.1.19/bin --blasr ~/software/blasr-1.3.1 --output-prefix operalg --output-directory $out
